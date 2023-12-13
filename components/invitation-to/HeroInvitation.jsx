dd0fe99729c18276d39aa3c4393b0ab3799bf475
Yosuandikaa <yosuaandika08@gmail.com>
2023-12-13 13:32:48 +0700

    fix shares

--- components/invitation-to/HeroInvitation.jsx
+++ components/invitation-to/HeroInvitation.jsx
@@ -5,7 +5,9 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import axios from "axios";
 import { toast } from "react-toastify";
 import apiService from "../../services/apiService";
-import shareWithNavigator, { shareVideoWithNavigator } from "../../lib/shareNavigator";
+import shareWithNavigator, {
+  shareVideoWithNavigator,
+} from "../../lib/shareNavigator";
 
 export default function HeroInvitation() {
   const router = useRouter();
@@ -25,7 +27,7 @@ export default function HeroInvitation() {
       const response = await apiService.post(
         // `https://peter-sheba.vercel.app/api/username`,
         `/api/username`,
-        { name },
+        { name }
       );
       setName("");
       setSubmitSuccess(true);
@@ -38,7 +40,7 @@ export default function HeroInvitation() {
   const fetchLink = async () => {
     try {
       const response = await apiService.get(
-        "/api/username",
+        "/api/username"
         // "/api/username",
       );
       setUserLinks(response.data.data);
@@ -138,15 +140,15 @@ export default function HeroInvitation() {
       user,
       authorId: user?.id,
       performer: name,
-      caption: captions
+      caption: captions,
     });
   };
 
   return (
     <div className="">
-          <h3 className="font-bold text-lg text-center m-0 mt-6">
-          Tambah Tamu Undangan
-        </h3>
+      <h3 className="font-bold text-lg text-center m-0 mt-6">
+        Tambah Tamu Undangan
+      </h3>
       <div className="relative max-w-md md:mx-auto mt-8 mx-4 md:mt-16 px-6 py-12 md:p-10 bg-white rounded-2xl border border-slate-200">
         {/* <h3 className="font-bold text-lg text-center m-0 mb-4">
           Tambah Tamu Undangan
@@ -198,16 +200,17 @@ export default function HeroInvitation() {
 
       <div className="mt-8 px-4 md:px-24">
         <h2 className="text-sm md:text-lg font-semibold">
-          Daftar Tamu <span className="pl-1 text-gray-300">{userLinks.length}</span>
+          Daftar Tamu{" "}
+          <span className="pl-1 text-gray-300">{userLinks.length}</span>
         </h2>
-        <ul className="grid grid-cols-1 md:grid-cols-4 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
           {userLinks.map((data, index) => (
-            <li
+            <div
               key={index}
               className="px-4 py-8 rounded-lg bg-white border border-slate-200 flex flex-col space-y-4 flex-wrap w-full max-w-full overflow-hidden"
             >
               <h3 className="font-bold text-lg">{data?.name}</h3>
-              <span className="text-sm font-light text-gray-600 block">{`${
+              <span className="text-sm font-light text-gray-600 block w-full max-w-full">{`${
                 window.location.origin
               }/invitation-to?nama=${encodeURIComponent(data?.name)}`}</span>
               {/* <CopyToClipboard
@@ -222,23 +225,39 @@ export default function HeroInvitation() {
                   Salin URL
                 </button>
               </CopyToClipboard> */}
-               <button
-                  className="bg-[#353434] text-white px-2 py-1 rounded-full"
-                  onClick={async () => {
-                    shareVideoWithNavigator({
-                      link: `${
-                        typeof window !== "undefined" && window.location.origin
-                      }/invitation-to?nama=${encodeURIComponent(data?.name)}`,
-                      performer: data?.name,
-                
-                    });
-                  }}                  
-                >
-                  Salin URL
-                </button>
-            </li>
+              <button
+                className="bg-[#353434] text-white px-2 py-1 rounded-full"
+                // onClick={async () => {
+                //   shareVideoWithNavigator({
+                //     link: `${
+                //       typeof window !== "undefined" && window.location.origin
+                //     }/invitation-to?nama=${encodeURIComponent(data?.name)}`,
+                //     performer: data?.name,
+
+                //   });
+                // }}
+                onClick={async () => {
+                  shareWithNavigator({
+                    // title: "",
+                    text: `${performer}
+        Check out my music video! Make sure to vote for it often so I can win the opportunity to perform at the wePOP come together on 6 August 2023.
+        to this link
+        ${link}
+
+        It's Easy! Click this link, vote for me now, and you too can win amazing weekly prizes:
+
+        THANKS!`,
+                    link: `${
+                      typeof window !== "undefined" && window.location.origin
+                    }/invitation-to?nama=${encodeURIComponent(data?.name)}`,
+                  });
+                }}
+              >
+                Salin URL
+              </button>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
